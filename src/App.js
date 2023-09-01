243a3f7d7857e680ba2b9f06aa6af6ad0cdbe9ad
FaizNaeem <faiznaeem192022@gmail.com>
2023-09-01 22:27:18 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './com/Header';
+import Footer from './com/Footer';
 // Header
 
 function App() {
   return (
     <div>
   <Header title="Code With Faiz" find={true}/>
+  <Footer />
     </div>
   );
 }
---
+++ src/com/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+        <h1>
+            Hi I Am Footer
+            </h1>
+            </div>
+  )
+}
